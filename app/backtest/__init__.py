932eeb392655606ccda947c80bd3b81b80797d76
arindam8423-mama <arindam8423@gmail.com>
2026-09-12 19:29:31 +1000

    Add backtest package exports

---
+++ app/backtest/__init__.py
@@ -0,0 +1,4 @@
+from app.backtest.engine import BacktestConfig, run_backtest
+from app.backtest.models import BacktestResult, Trade
+
+__all__ = ["BacktestConfig", "BacktestResult", "Trade", "run_backtest"]
